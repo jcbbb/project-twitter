3130a5555223b36ce75990f17efab6989122bd29
jc <avaz28082000@gmail.com>
2020-05-20 23:39:09 +0500

    Almost finished home page

--- client/src/components/nav/Nav.js
+++ client/src/components/nav/Nav.js
@@ -28,32 +28,45 @@ const Nav = () => {
                 <Navlink to="/explore" icon={<HashtagIcon />}>
                     Explore
                 </Navlink>
-                <Navlink icon={<NotificationIcon />}>Notifications</Navlink>
-                <Navlink icon={<MessageIcon />}>Messages</Navlink>
-                <Navlink icon={<BookmarkIcon />}>Bookmarks</Navlink>
-                <Navlink icon={<ListsIcon />}>Lists</Navlink>
-                <Navlink icon={<ProfileIcon />}>Profile</Navlink>
-                <Navlink icon={<MoreIcon />}>More</Navlink>
-                <Button className="button__filled" style={{ padding: '14px', width: '90%' }}>
+                <Navlink to="/notifications" icon={<NotificationIcon />}>
+                    Notifications
+                </Navlink>
+                <Navlink to="/messages" icon={<MessageIcon />}>
+                    Messages
+                </Navlink>
+                <Navlink to="/bookmarks" icon={<BookmarkIcon />}>
+                    Bookmarks
+                </Navlink>
+                <Navlink to="/lists" icon={<ListsIcon />}>
+                    Lists
+                </Navlink>
+                <Navlink to="/profile" icon={<ProfileIcon />}>
+                    Profile
+                </Navlink>
+                <Navlink to="/more" icon={<MoreIcon />}>
+                    More
+                </Navlink>
+                <Button
+                    className="button__filled nav__tweet-btn"
+                    style={{ padding: '14px', width: '90%' }}
+                >
                     Tweet
                 </Button>
-                <div className="nav__profile-container">
-                    <div className="nav__profile">
-                        <div className="nav__profile-image">
-                            <img src={ProfileImage} alt="Profile image" />
-                        </div>
-                        <div className="nav__profile-info">
-                            <div className="nav__profile-name-container">
-                                <span className="nav__profile-name">Jane Doe</span>
-                            </div>
-                            <div className="nav__profile-handle-container">
-                                <span className="nav__profile-handle">@janedoeatlocalhost.com</span>
-                            </div>
-                        </div>
-                        <span className="nav__profile-icon">
-                            <ChevronIcon />
-                        </span>
+                <div className="nav__profile">
+                    <div className="nav__profile-image">
+                        <img src={ProfileImage} alt="Profile image" />
                     </div>
+                    <div className="nav__profile-info">
+                        <div className="nav__profile-name-container">
+                            <span className="nav__profile-name">Jane Doe</span>
+                        </div>
+                        <div className="nav__profile-handle-container">
+                            <span className="nav__profile-handle">@janedoeatlocalhost.com</span>
+                        </div>
+                    </div>
+                    <span className="nav__profile-icon">
+                        <ChevronIcon />
+                    </span>
                 </div>
             </div>
         </nav>
